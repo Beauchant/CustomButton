d53203786eda2dfa3c8e08727ab6ce7a50c9bdbe
Enock Beauchant <beauchant509@gmail.com>
2022-11-26 14:28:11 -0500

    update2

--- app/src/main/java/ue/project/custombutton/MainActivity.java
+++ app/src/main/java/ue/project/custombutton/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Block screen orientation
+        // this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
